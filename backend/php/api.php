f4b983dbce52e197087887a82fdca182aa2c4794
AdrianFillippo <adrifillip2@gmail.com>
2022-04-22 21:30:25 -0500

    Json terminados

--- backend/php/api.php
+++ backend/php/api.php
@@ -1,8 +1,25 @@
 <?php
     header("Content-Type: application/json");
-    define('host', (string) 'backend/');
-    define('carpeta', (string) 'json/');
-    $peticion = file_get_contents($_SERVER["HTTP_REFERER"].host.carpeta."menu.json");
-    echo $peticion; 
+    require 'config.php';
+
+    $json = (object) [];
+    $json->menu = (object) $abrir(
+        archivo: "menu.json"
+    );
+    $json->inicio = (object) $abrir(
+        archivo: "inicio.json"
+    );
+    $json->ourmenu = (object) $abrir(
+        archivo: "ourmenu.json"
+    );
+    $json->ourkitchenExpert = (object) $abrir(
+        archivo: "ourkitchenExpert.json"
+    );
+    $json->theysaidaboutus = (object) $abrir(
+        archivo: "theysaidaboutus.json"
+    );
+    
+    echo json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
 
 ?>
